e9258c7bcf4c95d45fe3a4179abb1e1c182f3eb0
Luis Fernando Machado <>
2022-12-22 00:21:13 -0300

    Conditional When, with attribution

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,12 @@
 fun main(args: Array<String>) {
     conditional_When()
+    conditional_When_with_attribution()
 }
 
 fun conditional_When() {
     var option = 10
+
+    println("Simple Conditional When")
 
     when (option) {
         1 -> println("Option select, One")
@@ -17,7 +20,29 @@ fun conditional_When() {
         9 -> println("Option select, Nine")
         else -> {
             println("Unknown option")
-            println("Select new option...")
+            println("Select new option...\n")
+
         }
     }
+}
+
+fun conditional_When_with_attribution() {
+    var option = 10
+
+    println("Conditional When, with attribution")
+
+    var message = when (option) {
+        1 -> "Option select, One"
+        2 -> "Option select, Two"
+        3 -> "Option select, Three"
+        4 -> "Option select, Four"
+        5 -> "Option select, Five"
+        6 -> "Option select, Six"
+        7 -> "Option select, Seven"
+        8 -> "Option select, Eight"
+        9 -> "Option select, Nine"
+        else -> "Unknown option"
+    }
+
+    println(message)
 }
